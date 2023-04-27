5ae00d9a8a705bc281de03e268efcddc0efda471
chanikuk565 <92314686+chanikuk565@users.noreply.github.com>
2023-04-28 00:49:30 +0300

    Add files via upload

---
+++ CrimeMapper/enaliza_1.py
@@ -0,0 +1,41 @@
+import geopandas as gpd
+import pandas as pd
+import time
+# task 0
+# Read the CSV file
+df = gpd.read_file('statisticalareas_2020_demography.gdb')
+df_crime = pd.read_csv('filter.csv')
+# Join the statistical areas map file to crime data
+df_final_join = df.merge(df_crime, left_on='YISHUV_STAT11', right_on='StatArea')
+# ////////////////////////////////////////////////////////////
+# task1 sum
+# ///////////////////////////////////////////////////////
+# Group the rows by the yishuv_stat11 and StatisticCrimeGroup columns
+# and calculate the sum of the TikimSum column
+df_sum = df_final_join.groupby(['YISHUV_STAT11', 'StatisticCrimeGroup'])['TikimSum'].sum().reset_index()
+# Rename the column with the sum of TikimSum to tikim_sum
+df_sum = df_sum.rename(columns={'TikimSum': 'tikim_sum'})
+# ////////////////////////////////////////////////////////////
+# task2 max
+# ///////////////////////////////////////////////////////
+#  maximum value of 'tikim_sum' for each 'yishuv_stat11'
+miss_namegroup = df_sum.groupby('YISHUV_STAT11')['tikim_sum'].max().reset_index()
+# Rename the 'tikim_sum' column to 'tikim_max'
+miss_namegroup = miss_namegroup.rename(columns={'tikim_sum': 'tikim_max'})
+# Join the 'miss_namegroup' and 'sum_tikim' dataframes
+max_tikim=pd.merge(miss_namegroup, df_sum, left_on= ['YISHUV_STAT11', 'tikim_max'],right_on= ['YISHUV_STAT11', 'tikim_sum'], how = 'left')
+# Join the 'max_tikim' and 'df' dataframes
+to_geojason = df.merge(max_tikim, left_on='YISHUV_STAT11', right_on='YISHUV_STAT11')
+# setting the geometry column of the dataframe to be used as the geospatial data
+gdf = to_geojason.set_geometry('geometry')
+# changing the coordinate reference system of the dataframe
+gdf_crs84 = gdf.to_crs(epsg=4326)
+# saving the dataframe as a geojson file
+gdf_crs84.to_file("enaliza1_geopendes_11.geojson", driver='GeoJSON')
+
+
+
+
+
+
+
---
+++ CrimeMapper/enaliza_2.py
@@ -0,0 +1,57 @@
+import geopandas as gpd
+import pandas as pd
+import time
+# Read the GeoJSON file
+police_stations = gpd.read_file('PoliceStationBoundaries.geojson')
+# Read the CSV file
+df_crime = pd.read_csv('filter.csv')
+# Read the gdb formet
+gdb_2020=gpd.read_file('statisticalareas_2020_demography.gdb')
+# join 2020 whith crime
+df_final_join = gdb_2020.merge(df_crime, left_on='YISHUV_STAT11', right_on='StatArea')
+# creates a GeoDataFrame from the "police_stations" dataframe and sets the column 'geometry' as the geometry column
+gdf_police_stations = gpd.GeoDataFrame(police_stations, geometry='geometry')
+# reates a new column in the gdb_2020 dataframe named 'centroid_column' and sets it equal to the centroid of each feature in the gdb_2020 dataframe
+gdb_2020['centroid_column'] = gdb_2020.centroid
+# sets the 'centroid_column' as the geometry column for the gdb_2020 dataframe
+gdb_2020 = gdb_2020.set_geometry('centroid_column')
+# re-projects the gdf_police_stations dataframe to the coordinate reference system of the gdb_2020 dataframe
+df_crs = gdf_police_stations.crs
+gdb_2020_crs = gdb_2020.crs
+df_reprojected = gdf_police_stations.to_crs(gdb_2020_crs)
+# joins the two dataframes
+police_for_stt=gpd.sjoin(df_reprojected,gdb_2020,how='left',op="contains") 
+# join  whith crime
+result= df_crime.merge(police_for_stt, left_on='StatArea', right_on='YISHUV_STAT11')
+# task1 sum
+# ///////////////////////////////////////////////////////
+# Group the rows by the yishuv_stat11 and StatisticCrimeGroup columns
+# and calculate the sum of the TikimSum column
+df_sum = result.groupby(['tahananame', 'StatisticCrimeGroup'])['TikimSum'].sum().reset_index()
+#  # Rename the column with the sum of TikimSum to tikim_sum
+df_sum = df_sum.rename(columns={'TikimSum': 'tikim_sum'})
+start_time=time.time()
+# task2 max
+# ///////////////////////////////////////////////////////
+miss_namegroup = df_sum.groupby('tahananame')['tikim_sum'].max().reset_index()
+# Rename the 'tikim_sum' column to 'tikim_max'
+miss_namegroup = miss_namegroup.rename(columns={'tikim_sum': 'tikim_max'})
+# Join the 'miss_namegroup' and 'sum_tikim' dataframes using geopandas.merge()
+max_tikim=pd.merge(miss_namegroup, df_sum, left_on= ['tahananame', 'tikim_max'],right_on= ['tahananame', 'tikim_sum'], how = 'left')
+# joins the two dataframes
+to_geojason = gdf_police_stations.merge(max_tikim, left_on='tahananame', right_on='tahananame')
+# export to geojason file
+to_geojason.to_file("analiza2_geopwndes.geojson", driver='GeoJSON')
+# print("------% s-----" %(time.time() - start_time) )
+
+
+
+
+
+
+
+
+
+
+
+
